cfb7dee5a96de1b6eab8dee45c50ba1e9907fd76
Pizzanakin <lpizzanakinl@gmail.com>
2018-07-23 12:09:33 +0200

    added camera

---
+++ Assets/Scripts/Elements/Camera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Camera : MonoBehaviour {
+
+    public WebCamTexture mCamera;
+    public Image iCamera;
+
+    private Texture2D tex;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Script has been started");
+
+        mCamera = new WebCamTexture();
+        mCamera.Play();
+        Debug.Log("test");
+        tex = new Texture2D(mCamera.width, mCamera.height);
+        IntPtr pointer = mCamera.GetNativeTexturePtr();
+        tex.UpdateExternalTexture(pointer);
+
+        iCamera.sprite = Sprite.Create(tex, new Rect(0, 0, mCamera.width, mCamera.height), new Vector2(0, 0));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
